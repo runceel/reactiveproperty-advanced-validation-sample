d29c52105ff3735611836cca1160763427ba10b0
Kazuki Ota <kaota@microsoft.com>
2018-09-20 16:51:01 +0900

    impl

--- RPValidationSample/MainWindow.xaml.cs
+++ RPValidationSample/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Reactive.Bindings;
 
 namespace RPValidationSample
 {
@@ -23,6 +27,50 @@ namespace RPValidationSample
         public MainWindow()
         {
             InitializeComponent();
+        }
+    }
+
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private readonly DummyModel _model = new DummyModel();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [Required(ErrorMessage = "Input is required.")]
+        public ReactiveProperty<string> Input { get; }
+
+        public ReadOnlyReactivePropertySlim<string> ErrorMessage { get; }
+
+        public ReactiveCommand SubmitCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            Input = new ReactiveProperty<string>()
+                .SetValidateAttribute(() => Input)
+                .SetValidateNotifyError(_ => _model.Input.GetErrors(nameof(ReactiveProperty<string>.Value)));
+            _model.Input.ObserveErrorChanged.Subscribe(_ => Input.ForceValidate());
+
+            ErrorMessage = Input.ObserveErrorChanged
+                .Select(x => x?.OfType<string>().FirstOrDefault())
+                .ToReadOnlyReactivePropertySlim();
+
+            SubmitCommand = new ReactiveCommand()
+                .WithSubscribe(() =>
+                {
+                    _model.Input.Value = Input.Value;
+                });
+        }
+    }
+
+    public class DummyModel
+    {
+        [Range(0, 10, ErrorMessage = "0-10")]
+        public ReactiveProperty<string> Input { get; }
+
+        public DummyModel()
+        {
+            Input = new ReactiveProperty<string>()
+                .SetValidateAttribute(() => Input);
         }
     }
 }
